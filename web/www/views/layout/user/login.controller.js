f2a14d44fba8395e106d7248db9edb7ff55345b6
zhoujianqian <1744093040@qq.com>
2017-05-23 10:11:41 +0800

    界面设计

--- web/www/views/layout/user/login.controller.js
+++ web/www/views/layout/user/login.controller.js
@@ -4,7 +4,16 @@
 angular.module('', ['ui.router','starter.controllers'])
 .controller('LoginCtrl', function ($scope, $state) {
     $scope.toMain = function () {
-        console.log('fgdgfd');
+
+        $http({
+            method:'GET'
+            ,url:'${pageContext.request.contextPath}/login.do?" + "username=" + username+"&password="+password'
+        }).success(function () {
+            
+        }).error(function () {
+            
+        });
+        //console.log('fgdgfd');
         $state.go('app.main');
     };
 });
